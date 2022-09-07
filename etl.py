822794a1881d55ff72089c45b2301a00ebba173f
natalianrs <natalia.soaresnr@gmail.com>
2022-09-07 03:02:52 -0300

    Initial Commit

--- etl.py
+++ etl.py
@@ -4,21 +4,18 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
-#Load data from files stored in S3 to the staging tables using the queries declared on the sql_queries script
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
-#Select and Transform data from staging tables into the dimensional tables using the queries declared on the sql_queries script
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
-#Extract songs metadata and user activity data from S3, transform it using a staging table, and load it into dimensional tables for analysis    
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
--- sql_queries.py
+++ sql_queries.py
@@ -5,6 +5,11 @@ import configparser
 config = configparser.ConfigParser()
 config.read('dwh.cfg')
 
+ARN             = config.get('IAM_ROLE', 'ARN')
+LOG_DATA        = config.get('S3', 'LOG_DATA')
+LOG_JSONPATH    = config.get('S3', 'LOG_JSONPATH')
+SONG_DATA       = config.get('S3', 'SONG_DATA')
+
 # DROP TABLES
 
 staging_events_table_drop = "DROP TABLE IF EXISTS stagingevents;"
@@ -14,12 +19,6 @@ user_table_drop = "DROP TABLE IF EXISTS users;"
 song_table_drop = "DROP TABLE IF EXISTS songs;"
 artist_table_drop = "DROP TABLE IF EXISTS artists;"
 time_table_drop = "DROP TABLE IF EXISTS time;"
-
-
-ARN             = config.get('IAM_ROLE', 'ARN')
-LOG_DATA        = config.get('S3', 'LOG_DATA')
-LOG_JSONPATH    = config.get('S3', 'LOG_JSONPATH')
-SONG_DATA       = config.get('S3', 'SONG_DATA')
 
 # CREATE TABLES
 
@@ -200,7 +199,6 @@ time_table_insert = ("""INSERT INTO time (start_time,
     WHERE se.page = 'NextSong';""")
 
 # QUERY LISTS
-
 
 create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
